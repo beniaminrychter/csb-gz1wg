ed74583b243bb8ce7f6d68e5e5292f135441e004
Code Sandbox <hello@codesandbox.io>
2021-08-24 13:26:09 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.15e3a19b7ce455c3b13ca13ae52777c2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101dbfa27cb9dbd277347825c6692dee",
+    "url": "https://.github.io/csb-gz1wg/index.html"
+  },
+  {
+    "revision": "ccf5468ed51c90c1dbff",
+    "url": "https://.github.io/csb-gz1wg/static/js/2.4066beeb.chunk.js"
+  },
+  {
+    "revision": "67eef5e1c7d6a244aa27",
+    "url": "https://.github.io/csb-gz1wg/static/js/main.1a6df4eb.chunk.js"
+  },
+  {
+    "revision": "1c2ed449aa8e3275eb2a",
+    "url": "https://.github.io/csb-gz1wg/static/js/runtime~main.ee3cfae6.js"
+  }
+]);
